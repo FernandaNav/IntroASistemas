00b93ef0c8c09ba4c54ff06fdc785a92bb08fcb6
Fernabim <109820905+FernandaNav@users.noreply.github.com>
2024-05-08 09:20:24 -0600

    DOC TXT Y CAMBIOS EN MAIN
    He añadido lo que es el archivo info.txt con mis datos de estudiante y he modificado el archivo main.py porque le hacían falta otros datos y agregué datos de la universidad.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("¡Hola Inge!")
 print("Mi nombre es Fernanda Abigail Navarro Martínez.")
 print("Mi número de carné es: 1562724.")
+
+print("Datos adicionales:")
+print("Estudio la carrera de ingeniería en sistemas.")
+print("Datos de la Universidad:")
+print("Estudio en la Universidad Rafael Landívar Campus Quetzaltenango.")
